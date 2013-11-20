863905e282863d4c765a55f7055c8c4098441b18
Kyle Swanson <skierkyles@gmail.com>
2013-11-19 23:09:22 -0700

    Connect dialog start

--- python-client/main.py
+++ python-client/main.py
@@ -5,9 +5,15 @@ from gi.repository import Gtk
 
 class TISCaPClient:
     def __init__(self):
-        pass
+        self.uname = None
+        self.server = None
+        
+        self.builder = None
+        self.window = None
     
     def start(self): 
+        
+        
         self.builder = Gtk.Builder()
         self.builder.add_from_file("ui.glade")
         
@@ -30,30 +36,44 @@ class TISCaPClient:
     def show_login_dialog(self):
         #TODO Figure out why the dialog is blank when you click it a second time.
         content = self.builder.get_object("login_dialog_content")
+        content.show_all()
         
-        dialog = LoginDialog(self.window, content)
+        dialog = LoginDialog(self.window, content, self.builder.get_object("uname_entry"), self.builder.get_object("ip_entry"))
         res = dialog.run()
         
-        if res == Gtk.ResponseType.OK:
-            print "Connection times."
+        if res is not None:
+            self.uname = res[0]
+            self.server = res[1]
             
-        elif res == Gtk.ResponseType.CANCEL:
-            print "Not time to connect."
-        
         dialog.destroy()
         
     
 class LoginDialog(Gtk.Dialog):
-    def __init__(self, parent, content):
+    def __init__(self, parent, content, uname, server):
         Gtk.Dialog.__init__(self, "Connect to a Server", parent, 0, 
                             (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
                              Gtk.STOCK_CONNECT, Gtk.ResponseType.OK))
         self.set_default_size(300, 175)
-                            
+        
+        self.content = content
+        self.uname = uname
+        self.server = server
+        
         box = self.get_content_area()
-        box.add(content)
+        box.add(self.content)
         
         self.show_all()
+
+        
+    def run(self): 
+        result = super(LoginDialog, self).run()
+        
+        if result == Gtk.ResponseType.OK:
+            text = (self.uname.get_text(), self.server.get_text())
+        else:
+            text = None
+            
+        return text
     
 if __name__ == "__main__":
     client = TISCaPClient()
